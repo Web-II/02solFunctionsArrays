20f73f46150f9c1299f4bc9313e64548004aa2ef
FienSpriet <fien.spriet@gmail.com>
2022-03-10 12:47:12 +0100

    Extra uitbreiding ij toegevoegd

--- oefening 4/scripts/vertaalString.js
+++ oefening 4/scripts/vertaalString.js
@@ -1,6 +1,7 @@
 export function vertaal(zin) {
   let pzin = "",
     lettersTeHerhalen = "";
+
   for (const letter of zin) {
     if ("aeiouAEIOU".indexOf(letter) > -1) {
       lettersTeHerhalen += letter;
@@ -20,4 +21,35 @@ export function vertaal(zin) {
 
 function herhaal(zin, lettersTeHerhalen) {
   return (zin += "p" + lettersTeHerhalen);
+}
+
+export function vertaalIJ(zin) {
+  let pzin = "",
+    lettersTeHerhalen = "",
+    letter;
+
+  for (let i = 0; i < zin.length; i++) {
+    letter = zin.charAt(i);
+    if ("aeiouAEIOU".indexOf(letter) > -1) {
+      const volgendeLetter = zin.charAt(i + 1);
+      if (
+        letter.toLowerCase() === "i" &&
+        volgendeLetter.toLowerCase() === "j"
+      ) {
+        letter += volgendeLetter;
+        i++;
+      }
+      lettersTeHerhalen += letter;
+    } else {
+      if (lettersTeHerhalen.length > 0) {
+        pzin = herhaal(pzin, lettersTeHerhalen);
+        lettersTeHerhalen = "";
+      }
+    }
+    pzin += letter;
+  }
+  if (lettersTeHerhalen.length > 0) {
+    pzin = herhaal(pzin, lettersTeHerhalen);
+  }
+  return pzin;
 }
--- oefening 4/scripts/wesprekenp.js
+++ oefening 4/scripts/wesprekenp.js
@@ -1,9 +1,21 @@
-import { vertaal as vertaalString } from "./vertaalString.js";
-import { vertaal as vertaalArray } from "./vertaalArray.js";
+import {
+  vertaal as vertaalString,
+  vertaalIJ as vertaalIJString,
+} from "./vertaalString.js";
+import {
+  vertaal as vertaalArray,
+  vertaalIJ as vertaalIJArray,
+} from "./vertaalArray.js";
 
 const zin = prompt("Geef een zin in: ");
-//const zin="Als de kat van huis is, dansen de muizen";
+//const zin = "Als de kat van huis is, dansen de muizen";
 alert(`De zin in P taal: ${vertaalString(zin)}`);
 
+/* Oplossing voor extra oefening met IJ */
+alert(`De zin in P taal met ij: ${vertaalIJString(zin)}`);
+
 /* Oplossing gebruik makend van Arrays */
-//alert(`De zin in P taal: ${vertaalArray(zin)}`);
+alert(`De zin in P taal: ${vertaalArray(zin)}`);
+
+/* Oplossing voor extra oefening met IJ */
+alert(`De zin in P taal met ij: ${vertaalIJArray(zin)}`);
